4473c826ed4d4580dcb1a98778619b9aa993e1f6
William Oliphant <102601462+mimimimi87@users.noreply.github.com>
2024-11-15 10:28:54 -0600

    Add files via upload

--- src/view/aboutus.php
+++ src/view/aboutus.php
@@ -8,35 +8,28 @@
 </head>
 <body>
   <?php
-    include 'header.html'
+    include './components/header.html'
   ?>
   <div class="row" style="display:flex;">
   <div class="column" style="flex: 66.66%; padding:5px">
   <img src="../img/Screenshot.png" style="width: 100%">
   </div>
   <div class="column" style="flex: 66.66%;padding: 5px">
-  
-  
-  
-
   <h1>About Us</h1>
   <p>_________________________________________________________________________________________________________________________________________</p>
   <h2>We specialize in houseplants, planters, and soil amendments! Swing by and check out Springfield's tiniest plant shop! Let our plant obsession be your plant obsession </h2><BR>
   <h2> Details</h2>
   <P>_________________________________________________________________________________________________________________________________________</P>
   <h2>Grow for Good 417, LLC was started in 2021 focusing on plants and plant education. Growing up in rural Missouri to a family of educators, we did not have a lot of money. Each season our family grew our own produce. Farming with my father taught me an understanding of plants. Volunteering with my mom and grandmother at plant-related events played a role in my plant education. In college, I was the guy with all the plants. Many of my classes were focused on botany, leading me to a degree in biology, and I am currently a professor of biology at a local college. These experiences all culminated in a lifelong passion for plants and sharing them with others. 
-My ‘Pandemic Project’ was to build a greenhouse, providing the perfect opportunity to start a plant business in 2021. 
-Grow For Good 417 creates Garden Art pieces. We find unique containers at flea markets and thrift stores to repurpose and pair with the perfect plant, creating a piece of art that our customers proudly display in their home or office! 
-We work with local businesses to throw Plant pARTy classes and pass along the knowledge I have received to future green thumbs. People have fun, learn about plants, get together with friends, and create their own pieces of Garden Art.</h2><BR>
+      My ‘Pandemic Project’ was to build a greenhouse, providing the perfect opportunity to start a plant business in 2021. 
+      Grow For Good 417 creates Garden Art pieces. We find unique containers at flea markets and thrift stores to repurpose and pair with the perfect plant, creating a piece of art that our customers proudly display in their home or office! 
+      We work with local businesses to throw Plant pARTy classes and pass along the knowledge I have received to future green thumbs. People have fun, learn about plants, get together with friends, and create their own pieces of Garden Art.</h2><BR>
   <h1>Our location</h1>
   <p>________________________________________________________________________________________________________________________________________</p>
-
-  
   <h2>
   2463 W. Sunshine, Springfield, MO, United States, Missouri</h2>
   <img src="../img/Screenshot_14-11-2024_121053_www.facebook.com.jpeg" style="width: 40%" >
   </div>
-  
 </div>
  
 
